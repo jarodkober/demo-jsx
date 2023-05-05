07f91c96d5b13550eb3069812b5f32985584d7aa
Jarod Kober <jarod.development@gmail.com>
2023-05-05 15:26:37 -0700

    Updated HTML to Leverage Bulma Styling
    * Updated markup in the App and ProfileCard components to leverage Bulma component styling.
    * Created description prop.
    * Added descriptions for each profile.

--- src/App.js
+++ src/App.js
@@ -7,22 +7,36 @@ import SiriImage from './images/siri.png';
 function App() {
 	return (
 		<div>
-			<div>Personal Digital Assistants</div>
-			<ProfileCard
-				handle="@alexa99"
-				image={AlexaImage}
-				title="Alexa"
-			/>
-			<ProfileCard
-				handle="@cortana32"
-				image={CortanaImage}
-				title="Cortana"
-			/>
-			<ProfileCard
-				handle="@siri01"
-				image={SiriImage}
-				title="Siri"
-			/>
+			<section className="hero is-primary">
+				<div className="hero-body">
+					<p className="title">Personal Digital Assistants</p>
+				</div>
+			</section>
+
+			<div className="container">
+				<section className="section">
+					<div className="columns">
+						<ProfileCard
+							description="Alexa was created by Amazon and helps you buy things."
+							handle="@alexa99"
+							image={AlexaImage}
+							title="Alexa"
+						/>
+						<ProfileCard
+							description="Cortana was made by Microsoft. Who knows what it does?"
+							handle="@cortana32"
+							image={CortanaImage}
+							title="Cortana"
+						/>
+						<ProfileCard
+							description="Siri was made by Apple and is used across their ecosystem of products."
+							handle="@siri01"
+							image={SiriImage}
+							title="Siri"
+						/>
+					</div>
+				</section>
+			</div>
 		</div>
 	);
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,12 +1,24 @@
-function ProfileCard({ handle, image, title }) {
+function ProfileCard({ description, handle, image, title }) {
 	return (
-		<div>
-			<img
-				alt={title + ' logo'}
-				src={image}
-			/>
-			<div>Title is {title}</div>
-			<div>Handle is {handle}</div>
+		<div className="column is-4">
+			<div className="card">
+				<div className="card-image">
+					<figure className="image is-1by1">
+						<img
+							alt={title + ' logo'}
+							src={image}
+						/>
+					</figure>
+				</div>
+
+				<div className="card-content">
+					<div className="media-content">
+						<p className="title is-4">{title}</p>
+						<p className="subtitle is-6">{handle}</p>
+					</div>
+					<div className="content">{description}</div>
+				</div>
+			</div>
 		</div>
 	);
 }
