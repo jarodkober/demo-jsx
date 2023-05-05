06ab3a3b63e0ce798c280c96c340f68edcb2e774
Jarod Kober <jarod.development@gmail.com>
2023-05-05 14:34:06 -0700

    Added Images to the ProfileCard Component
    * Updated ProfileCard component to support images.
    * Added logos for each profile.
    * Imported images into the App component.
    * Correct prop formatting to be a single attribute per line and alphabetized.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App() {
 	return (
 		<div>
 			<div>Personal Digital Assistants</div>
-			<ProfileCard title="Alexa" handle="@alexa99" />
-			<ProfileCard title="Cortana" handle="@cortana32" />
-			<ProfileCard title="Siri" handle="@siri01" />
+			<ProfileCard
+				handle="@alexa99"
+				image={AlexaImage}
+				title="Alexa"
+			/>
+			<ProfileCard
+				handle="@cortana32"
+				image={CortanaImage}
+				title="Cortana"
+			/>
+			<ProfileCard
+				handle="@siri01"
+				image={SiriImage}
+				title="Siri"
+			/>
 		</div>
 	);
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,6 +1,10 @@
-function ProfileCard({ handle, title }) {
+function ProfileCard({ handle, image, title }) {
 	return (
 		<div>
+			<img
+				alt={title + ' logo'}
+				src={image}
+			/>
 			<div>Title is {title}</div>
 			<div>Handle is {handle}</div>
 		</div>
